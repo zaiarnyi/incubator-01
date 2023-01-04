20c0332ae2e90c1ac527eef451632e3bc5dbf516
Frontend_Denys_Z <denys.zaiarnyi@abz.agency>
2023-01-04 19:20:01 +0200

    feature: init project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, {Request, Response} from 'express';
-const app = express()
-const port = 3000
+
+const port = process.env.PORT || 3000
+const parseMiddleware = express.json();
+
+const app = express();
+app.use(parseMiddleware);
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
