78ab74af2961c19cde7c3269c91277d58583a289
Frontend_Denys_Z <denys.zaiarnyi@abz.agency>
2023-01-22 11:31:51 +0200

    feature: reset

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,7 @@ app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/html')
   res.send(`
 <h2>PORT: ${port}</h2>
+<h2>PORT: ${process.env.MONGO_DB_URL}</h2>
 <h3>Ukrainian time: ${new Date().toLocaleString("ua", {timeZone: "Europe/Kiev"})}</h3>
 `)
 })
