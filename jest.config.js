b1af2cf1418a67ea1513d1db18441fd05e62a4fa
Frontend_Denys_Z <denys.zaiarnyi@abz.agency>
2023-01-22 10:59:26 +0200

    feature: reset

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
-const { defaults: tsjPreset } = require('ts-jest/presets')
-
 module.exports = {
-  preset: "@shelf/jest-mongodb",
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: tsjPreset.transform,
 };
