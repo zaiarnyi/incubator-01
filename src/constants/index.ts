fcd558fd375c0d17cec6b159162924cf6c6f4c26
Frontend_Denys_Z <denys.zaiarnyi@abz.agency>
2023-01-28 21:15:03 +0200

    feature: finish home work 3

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const REQUIRED_FIELD = 'Required field'
 export const INVALID_VALUE = 'Invalid value'
 export const NOT_FOUND_BLOG_ID = 'Blog with this id was not found'
 export const MAX_LENGTH_VALUE = (number: number): string => `Maximum number of characters ${number}`
-export const DB_NAME_COLLECTION_BLOG = '_blogs';
+export const DB_NAME_COLLECTION_BLOG = 'blogs';
 export const DB_NAME_COLLECTION_PRODUCTS = 'products';
 
 
