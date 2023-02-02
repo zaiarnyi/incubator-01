f1cb6d8cddde03ad9001af431beaedde6858b154
Frontend_Denys_Z <denys.zaiarnyi@abz.agency>
2023-02-02 21:12:23 +0200

    fix: fixed bugs

--- src/utils/queryParamsForUsers.ts
+++ src/utils/queryParamsForUsers.ts
@@ -8,17 +8,20 @@ export const mappingQueryParamsUsers = (query: IQueryParamsUsers) : IQueryParams
   const sortDirection = query.sortDirection || 'desc';
   const pageNumber = +(query.pageNumber || 1);
   const limit = +(query.pageSize || DEFAULT_PAGE_SIZE);
-  const searchRegex = {$or: []};
-  if(searchLoginTerm.length){
-    const loginTerm = {login: {$regex: new RegExp(searchLoginTerm, 'gi')}};
-    // @ts-ignore
-    searchRegex.$or.push(loginTerm)
-  }
-  if(searchEmailTerm.length){
-    const emailTerm =  {email: {$regex: new RegExp(searchEmailTerm, 'gi')}}
-    // @ts-ignore
-    searchRegex.$or.push(emailTerm);
-  }
+  const searchRegex = {$or: [
+      {login: {$regex: new RegExp(searchLoginTerm, 'gi')}},
+      {email: {$regex: new RegExp(searchEmailTerm, 'gi')}}
+    ]};
+  // if(searchLoginTerm.length){
+  //   const loginTerm = {login: {$regex: new RegExp(searchLoginTerm, 'gi')}};
+  //   // @ts-ignore
+  //   searchRegex.$or.push(loginTerm)
+  // }
+  // if(searchEmailTerm.length){
+  //   const emailTerm =  {email: {$regex: new RegExp(searchEmailTerm, 'gi')}}
+  //   // @ts-ignore
+  //   searchRegex.$or.push(emailTerm);
+  // }
 // ...(searchLoginTerm.length && {login: {$regex: new RegExp(searchLoginTerm, 'gi')}}),
 // ...(searchEmailTerm.length && {email: {$regex: new RegExp(searchEmailTerm, 'gi')}})
 
