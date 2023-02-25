a8fb3776d5b80db3840d7bc497f4f7cdbab7913c
Frontend_Denys_Z <denys.zaiarnyi@abz.agency>
2023-02-25 15:58:49 +0200

    feature: added logic for like/dislike

--- src/constants/token.ts
+++ src/constants/token.ts
@@ -3,5 +3,5 @@ export const TOKEN_EXPIRE_TIME = {
   accessToken: 10
 }
 
-export const HTTPS_ONLY_COOKIES = false;
-export const SECURITY_COOKIE = false;
+export const HTTPS_ONLY_COOKIES = true;
+export const SECURITY_COOKIE = true;
